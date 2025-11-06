13dbe60bb0dc9d4cbacd81687c48eb73eabf9d5b
Braa Refaat <braarefat2002@gmail.com>
2025-11-06 13:07:12 +0200

    upload files for Bootstrap Version

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // Navbar and Footer
+    $(this).find(".fa-plus").show();
+    $(this).find(".fa-minus").hide();
+    $(".toggleButton, .toggleButtonHeader").click(function () {
+        if ($(window).width() <= 1280) {
+            let dropdown;
+
+            // Footer
+            if ($(this).closest(".footer-item").length) {
+                dropdown = $(this).closest("div").next("ul");
+            }
+
+            // Header
+            if ($(this).closest("div").find(".dropdown-menu-news").length) {
+                dropdown = $(this).closest("div").find(".dropdown-menu-news");
+            }
+            dropdown.toggleClass("show");
+
+            if (dropdown.hasClass("show")) {
+                $(this).find(".fa-plus").hide();
+                $(this).find(".fa-minus").show();
+            } else {
+                $(this).find(".fa-plus").show();
+                $(this).find(".fa-minus").hide();
+            }
+        }
+    });
+
+    // Search
+    $(".search-btn").on("click", function (e) {
+        e.preventDefault();
+        $(".searchBarOpen").addClass("active");
+        $("body").addClass("overflow-hidden");
+    });
+
+    $(".searchBarOpen--closeBtn").on("click", function () {
+        $(".searchBarOpen").removeClass("active");
+        $("body").removeClass("overflow-hidden");
+    });
+});
+
